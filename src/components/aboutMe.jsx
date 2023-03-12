61ed22b34475206778a9bce6f3c04a51426b4059
Akhil <akhilpdominic2001@gmail.com>
2023-03-12 14:27:09 +0530

    completed aboutme

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -7,14 +7,21 @@ const About = () =>{
         <div className='bg-white w-full py-16 px-4'>
             <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
                 
-                    <img src={devimg} alt="/" />
-               <div className='py-6'>
-                <p>Hi</p>
+                    <img className='w-[300px] mx-auto my-4' src={devimg} alt="/" />
+               <div className='flex flex-col justify-center'>
+                <p className='font-mono font-bold text-2xl'>Hi</p>
+                <br></br>
+                <p className='font-mono text-lg'>
                 I am a computer science student currently in my senior year. 
                 My passion for computer science from a very young age led me to taking up
                 Computer science as my field of study. Feel free to take a look at my side projects
                 and if you have a suitable opening for me, please let me know 😊.
-               </div>
+               
+                </p>
+                <button className='bg-orange-600 w-[200px] text-white rounded-md font-medium my-6 mx-auto py-3'>
+            Get started
+        </button>
+                </div>
                 
 
             </div>
